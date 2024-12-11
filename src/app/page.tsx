68398bdd207c9eaf17b96102bf4375d02d290142
Joseph Chretien-Golden <joechre@gmail.com>
2024-12-10 18:38:23 -0600

    landing page complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import NonDashboardNavbar from "@/components/NonDashboardNavbar";
+import Landing from "@/app/(nondashboard)/landing/page";
+import Footer from "@/components/Footer";
+
+
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-   hi
-    </div>
+    <div className="nondashboard-layout">
+   <NonDashboardNavbar />
+      <main className="nondashboard-layout__main">
+        <Landing />
+      </main>  
+      <Footer />
+      </div>
   );
 }
